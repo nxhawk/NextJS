98bdcf00daeb3afcafe7f10c37b6839f6a7163f1
21120447 <haonhat2729@gmail.com>
2023-11-17 22:02:20 +0700

    todo list app

---
+++ todo/components/Task.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { ITask } from "@/types/task";
+import React, { SyntheticEvent } from "react";
+interface TaskProps {
+  task: ITask;
+}
+
+import { FormEventHandler, useState } from "react";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
+import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import { deleteTodo, editTodo } from "@/api";
+
+const Task = ({ task }: TaskProps) => {
+  const router = useRouter();
+  const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
+  const [openModalDeleted, setOpenModalDeleted] = useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
+
+  const handleSubmitEditTodo = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id,
+      text: taskToEdit,
+    });
+    setOpenModalEdit(false);
+    router.refresh();
+  };
+
+  const handleDeleteTask = async (id: string) => {
+    await deleteTodo(id);
+    setOpenModalDeleted(false);
+    router.refresh();
+  };
+
+  return (
+    <tr>
+      <td className="w-full">{task.text}</td>
+      <td className="flex gap-5">
+        <FiEdit
+          cursor="pointer"
+          onClick={() => setOpenModalEdit(true)}
+          size={25}
+          className="text-blue-500"
+        />
+        <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+          <form onSubmit={handleSubmitEditTodo}>
+            <h3 className="font-bold text-lg">Edit task</h3>
+            <div className="modal-action">
+              <input
+                value={taskToEdit}
+                onChange={(e) => setTaskToEdit(e.target.value)}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full"
+              />
+              <button type="submit" className="btn">
+                Submit
+              </button>
+            </div>
+          </form>
+        </Modal>
+        <FiTrash2
+          cursor="pointer"
+          size={25}
+          onClick={() => setOpenModalDeleted(true)}
+          className="text-red-500"
+        />
+        <Modal modalOpen={openModalDeleted} setModalOpen={setOpenModalDeleted}>
+          <h3 className="text-lg">
+            Are you sure, you want to delete this task?
+          </h3>
+          <div className="modal-action">
+            <button onClick={() => handleDeleteTask(task.id)} className="btn">
+              Yes
+            </button>
+          </div>
+        </Modal>
+      </td>
+    </tr>
+  );
+};
+
+export default Task;
---
+++ todo/components/TodoList.tsx
@@ -0,0 +1,30 @@
+import { ITask } from "@/types/task";
+import React from "react";
+import Task from "./Task";
+
+interface TodoListProps {
+  tasks: ITask[];
+}
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          {tasks.map((task) => (
+            <Task key={task.id} task={task} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TodoList;
